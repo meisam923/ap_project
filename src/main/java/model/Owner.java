a86b9e657e06663ac6b5b8b98ce678dbcafe272f
rezaj123 <rezamh823@gmail.com>
2025-04-28 22:14:10 +0330

    added UserManager(singleton) class + gave each User a UniqueId + Owner is associated with a restaurant

--- src/main/java/model/Owner.java
+++ src/main/java/model/Owner.java
@@ -4,10 +4,12 @@ public class Owner extends User {
     private Address address;   // human-readable address (not used for distance)
     private Location location; // a coordinate system
 
+    private final Restaurant restaurant;
 
-    public Owner(String first_name, String last_name, String phone_number, String email, String password, Role role, Address address, Location location) {
+    public Owner(String first_name, String last_name, String phone_number, String email, String password, Role role, Address address, Location location, Restaurant restaurant) {
         super(first_name, last_name, phone_number, email, password, role);
         this.address = address;
         this.location = location;
+        this.restaurant = restaurant;
     }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,6 +1,10 @@
 package model;
 
+import java.util.UUID;
+
 public abstract class User {
+
+    private final String publicId; // UUID used for referencing in collections
 
     private String first_name;
     private String last_name;
@@ -10,6 +14,7 @@ public abstract class User {
     private Role role;
 
     public User(String first_name, String last_name, String phone_number, String email, String password, Role role) {
+        this.publicId = UUID.randomUUID().toString();
 
         this.first_name = first_name;
         this.last_name = last_name;
@@ -17,8 +22,47 @@ public abstract class User {
         this.email = email;
         this.password = password;
         this.role = role;
-
     }
 
+    // Getters and setters
+    public String getPublicId() {
+        return publicId;
+    }
+    public String getFirstName() {
+        return first_name;
+    }
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLastName() {
+        return last_name;
+    }
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+    public void setPhoneNumber(String phone_number) {
+        this.phone_number = phone_number;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
 }
